202e6904a81d6229b9ba2197838dff0290110228
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2022-10-04 22:00:58 -0400

    added redux to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Container, AppBar, Typography, Grid, Grow} from "@material-ui/core";
+import {useDispatch} from "react-redux";
 
+import {getPosts} from './actions/posts'
 import memories from './images/memories.png';
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
 import useStyles from './styles';
 
+
+
 const App = () => {
     const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts())
+    },[dispatch])
+
     return (
         <Container maxWidth='lg'>
             <AppBar className={classes.appBar} position='static' color='inherit'>
@@ -20,7 +30,7 @@ const App = () => {
                         <Grid item xs={12} sm={7}>
                             <Posts/>
                         </Grid>
-                        <Grid item xs={12} sm={7}>
+                        <Grid item xs={12} sm={4}>
                             <Form/>
                         </Grid>
                     </Grid>
--- server/index.js
+++ server/index.js
@@ -9,12 +9,12 @@ import postRoutes from './routes/posts.js';
 dotenv.config()
 
 const app = express();
+app.use(cors());
 
 app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
-app.use(cors());
 
 
 const PORT = process.env.PORT || 3010;
