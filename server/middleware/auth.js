c5453358589c8f6c41c16cf493bf8aade5c60edd
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2022-10-06 19:40:06 -0400

    added middleware for protected routes on server. Updated likes with auth functionality

---
+++ server/middleware/auth.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+
+const Auth = async (req,res,next) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        const isCustomAuth = token.length < 500;
+
+        let decodedData;
+
+        if(token && isCustomAuth) {
+            decodedData = jwt.verify(token, 'this-is-my-secret-key');
+
+            req.userId = decodedData?.id;
+        } else {
+            decodedData = jwt.decode(token);
+
+            req.userId = decodedData?.sub;
+        }
+
+        next();
+    } catch (e) {
+        console.log(e)
+    }
+};
+
+export default Auth;
