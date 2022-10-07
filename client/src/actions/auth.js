dd6a51bca3f74e09e2dd170d548ef4bead004503
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2022-10-06 20:05:08 -0400

    added the ability to signup/signin from the client and store user in the mongodb

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -5,7 +5,9 @@ import * as api from '../api/index.jsx';
 export const signin = (formData, history) => async (dispatch) => {
     try {
         // log in the user
+        const {data} = await api.signIn(formData);
 
+        dispatch({type: AUTH, data})
         history.push('/')
     } catch (error) {
         console.log(error)
@@ -15,6 +17,10 @@ export const signin = (formData, history) => async (dispatch) => {
 export const signup = (formData, history) => async (dispatch) => {
     try {
         // log up the user
+        const {data} = await api.signUp(formData);
+
+        dispatch({type: AUTH, data})
+
         history.push('/')
     } catch (error) {
         console.log(error)
