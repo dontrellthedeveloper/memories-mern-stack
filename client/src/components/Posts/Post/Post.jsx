7baaa8ddeb4e58492d09b7bd2c794512647ca617
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2022-10-07 04:42:59 -0400

    added likes to state so it updates instantly

--- client/src/components/Posts/Post/Post.jsx
+++ client/src/components/Posts/Post/Post.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Card, CardActions, CardContent, CardMedia, Button, Typography, ButtonBase } from '@material-ui/core';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
@@ -17,14 +17,30 @@ const Post = ({post, setCurrentId}) => {
     const dispatch = useDispatch();
     const history = useHistory();
     const classes = useStyles();
+    const [likes, setLikes] = useState(post?.likes);
+
+
+    const userId = user?.result.sub || user?.result?._id;
+    const hasLikedPost = likes.find((like) => like === userId);
+
+    const handleLike = async () => {
+        dispatch(likePost(post._id));
+
+        if (hasLikedPost) {
+            setLikes(likes.filter((id) => id !== userId));
+        } else {
+            setLikes([...likes, userId]);
+        }
+    };
+
 
     const Likes = () => {
-        if (post?.likes?.length > 0) {
-            return post.likes.find((like) => like === (user?.result?.sub || user?.result?._id))
+        if (likes.length > 0) {
+            return likes.find((like) => like === (user?.result?.sub || user?.result?._id))
                 ? (
-                    <><ThumbUpAltIcon fontSize="small" />&nbsp;{post.likes.length > 2 ? `You and ${post.likes.length - 1} others` : `${post.likes.length} like${post.likes.length > 1 ? 's' : ''}` }</>
+                    <><ThumbUpAltIcon fontSize="small" />&nbsp;{likes.length > 2 ? `You and ${likes.length - 1} others` : `${likes.length} like${likes.length > 1 ? 's' : ''}` }</>
                 ) : (
-                    <><ThumbUpAltOutlined fontSize="small" />&nbsp;{post.likes.length} {post.likes.length === 1 ? 'Like' : 'Likes'}</>
+                    <><ThumbUpAltOutlined fontSize="small" />&nbsp;{likes.length} {likes.length === 1 ? 'Like' : 'Likes'}</>
                 );
         }
 
@@ -74,7 +90,7 @@ const Post = ({post, setCurrentId}) => {
                 </CardContent>
             </ButtonBase>
             <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary" disabled={!user?.result} onClick={() => dispatch(likePost(post._id))}>
+                <Button size="small" color="primary" disabled={!user?.result} onClick={handleLike}>
                     <Likes />
                 </Button>
                 {(user?.result?.sub === post?.creator || user?.result?._id === post?.creator) && (
